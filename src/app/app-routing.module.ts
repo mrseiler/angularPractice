d2fd90a3e3e79ba525633185aee2437f45c9ded5
Mason Seiler <mseiler@finishline.com>
2018-12-26 15:52:29 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
 
 const routes: Routes = [
-  {path: 'carousel', component: CarouselComponent}
+  {path: 'carousel', component: CarouselComponent},
+  {path: 'expansion-panel', component: ExpansionPanelComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    ExpansionPanelComponent
   ],
   imports: [
     BrowserModule,
-    //NgbModule,
+    MatExpansionModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
